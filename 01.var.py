012be2d9d86a8793c3f023a45ae88a7eee13b272
decjiang <154988118@qq.com>
2022-09-24 11:54:25 +0800

    add 1st script

---
+++ 01.var.py
@@ -0,0 +1,34 @@
+#print("cisco devnet")
+
+v1 = "cisco devnet"
+print(v1)
+
+#v1_type = type(v1)
+#print(v1_type) # <class 'str'>
+
+print(type(v1)) # <class 'str'>
+
+v2 = 100
+print(type(v2)) #<class 'int'>
+
+v3 = 10.0
+print(type(v3)) #<class 'float'>
+
+v4 = False
+print(type(v4)) # <class 'bool'>
+
+v5 = (10 , 10.1 , "cisco" , "catalyst")
+print(type(v5)) # <class 'tuple'>
+
+v6 = [10 , 10.1 , "cisco" , "catalyst"]
+print(type(v6)) #<class 'list'>
+
+v7 = {10 , 10.1 , "cisco" , "catalyst"}
+print(type(v7)) # <class 'set'>
+
+v8 = {"name" : "zhangsan" , "age" : 18  , "com":"cisco"}
+print(type(v8)) # <class 'dict'>
+
+print(v1)
+print(print(v1))
+
